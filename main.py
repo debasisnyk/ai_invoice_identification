4c3637afda518ad4b83ecf45ca8dcf226b954956
Debasis Nayak <your_email@gmail.com>
2026-05-28 00:14:51 +0800

    request to gemini without function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import os
 
@@ -17,24 +18,53 @@ api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 # -----------------------------
-# Ask user input
+# TOOL DECLARATION
+# -----------------------------
+
+invoice_tool = {
+    "function_declarations": [
+        {
+            "name": "get_failed_invoices",
+            "description": "Get failed invoices from a country above a minimum amount",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "country": {
+                        "type": "STRING",
+                        "description": "Country name"
+                    },
+                    "min_amount": {
+                        "type": "INTEGER",
+                        "description": "Minimum invoice amount"
+                    }
+                },
+                "required": ["country", "min_amount"]
+            }
+        }
+    ]
+}
+
+# -----------------------------
+# User input
 # -----------------------------
 
 user_prompt = input("Ask something: ")
 
 # -----------------------------
-# Send prompt to Gemini
+# Send to Gemini
 # -----------------------------
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=user_prompt
+    contents=user_prompt,
+
+    config=types.GenerateContentConfig(
+        tools=[invoice_tool]
+    )
 )
 
 # -----------------------------
-# Print response
+# Print RAW response
 # -----------------------------
 
-print("\nGemini Response:\n")
-
-print(response.text)
+print(response)
